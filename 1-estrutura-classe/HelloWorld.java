1446a82acdf86d04b143d12cbf9ce185d40a6c4e
Henrique Ferreira <62857389+henferreirapro@users.noreply.github.com>
2023-10-28 15:24:51 -0300

    Criando uma classe em Java

---
+++ 1-estrutura-classe/HelloWorld.java
@@ -0,0 +1,18 @@
+
+
+/* Composição de uma classe Java 📋
+    * HelloWorld = nome da classe.
+    * public = tipo de acesso da classe.
+    * classe = tipo da classe.
+*/
+public class HelloWorld {
+
+  // todo conteudo que pertence ao escopo da nossa classe ✌️😉
+  public static void main(String[] args) {
+
+    // System.out serve como um console.log no Java
+    System.out.println("Ola, Mundo!");
+    
+  }
+
+}
