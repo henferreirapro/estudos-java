682c4ccd502da311ffdf30affabb9e8a0e6796f5
Henrique Ferreira <62857389+henferreirapro@users.noreply.github.com>
2023-10-28 16:03:36 -0300

    Criando os tipos de dados e varaiaveis em Java.
    Aqui temos um exemplo de como criamos variaveis e declaramos ela com os tipos principais de dados mais usados em Java.

---
+++ 1-estrutura-classe/TiposDados.java
@@ -0,0 +1,31 @@
+/*
+  * Valores numericos = (int, double, float, long).
+  * Texto = (String) com a primeira letra maiuscula.
+  * Booleanos/ = boolean (True e False)
+
+*/
+
+
+public class TiposDados {
+  public static void main(String[] args) {
+    
+    // Criando uma variavel do tipo int
+    int dadoDoTipoInt = 10;
+
+    // Criando uma variavel do tipo double
+    double dadoDoTipoDouble = 2.46;
+
+    // Criando uma variavel do tipo float
+    float dadoDoTipoFloat = 3.14f; //precisa add a letra"f" no final.
+
+    // Criando uma variavel do tipo long
+    long dadoDoTipoLong = 1234242542L; //precisa add a letra "L" no final.
+
+    // Criando uma variavel do tipo String
+    String dadoDoTipoString = "Henrique";
+
+    // Criando uma variavel do tipo boolean
+    boolean dadoDoTipoBoolean = true;
+
+  }
+}
