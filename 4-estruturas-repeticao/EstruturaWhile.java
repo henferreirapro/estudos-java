fd67cc867b375088d29bb29c08956c978d6d060a
Henrique Ferreira <62857389+henferreirapro@users.noreply.github.com>
2023-10-28 19:50:07 -0300

    Criando Estrutura de repetição while

---
+++ 4-estruturas-repeticao/EstruturaWhile.java
@@ -0,0 +1,17 @@
+
+
+public class EstruturaWhile {
+  public static void main(String[] args) {
+    int valorInicial = 0;
+
+    while (valorInicial < 5) {
+      System.out.println("O valor é: " + valorInicial);
+
+      // add +1 ao valor que está armazenado.
+      valorInicial++;
+      
+    }
+
+    System.out.println("While finalizado!");
+  }
+}
